a6f5e6546946b87df22e0c366609301ad8ebfa22
bek <shoyatbekov03032003@gmail.com>
2024-03-11 15:31:32 +0500

    vercel speedInsights added

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Home from "./pages/Home";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import AddQuiz from "./pages/AddQuiz";
 import QuestionView from "./components/QuizView";
 import QuizPage from "./pages/Quiz";
@@ -35,6 +36,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace={true} />} />
         </Routes>
       </main>
+      <SpeedInsights />
     </Router>
   );
 }
